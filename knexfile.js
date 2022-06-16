845323a55d041d5264202869da61887ede289392
viniciushrcs <viniciushrcs@gmail.com>
2022-06-16 19:03:57 -0300

    chore: config env, knex, postgres and make migration to create student table

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+const { postgres } = require('./src/config')
+
+module.exports = {
+  client: postgres.client,
+  connection: {
+    host: postgres.connection.host,
+    port: postgres.connection.port,
+    user: postgres.connection.user,
+    password: postgres.connection.password,
+    database: postgres.connection.database,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    directory: './src/infra/data/database/migrations',
+    tableName: 'knex_migrations',
+  },
+}
