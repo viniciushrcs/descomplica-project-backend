256042e56098cb438114e9f8ce66c7a11bf4d709
viniciushrcs <viniciushrcs@gmail.com>
2022-06-17 15:50:14 -0300

    feat: create typeDefs of graphQL and Student type

---
+++ src/app/graphql/_typeDefs/types.js
@@ -0,0 +1,16 @@
+const typeDefs = `
+  type Mutation {
+  }
+
+  type Query {
+  }
+
+  type Student {
+    id: ID
+    name: String
+    cpf: String
+    email: String
+  }
+`
+
+module.exports = typeDefs
