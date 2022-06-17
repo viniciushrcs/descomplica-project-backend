081ae3b96e75762d348a7e23a9dd752131b107e4
viniciushrcs <viniciushrcs@gmail.com>
2022-06-17 15:47:45 -0300

    fix: change createTableStudent to remove "unique" constraint of name

--- src/infra/database/migrations/20220616205207_CreateTableStudent.js
+++ src/infra/database/migrations/20220616205207_CreateTableStudent.js
@@ -1,10 +1,9 @@
 exports.up = (knex) =>
   knex.schema.createTable('student', (table) => {
     table.uuid('id').primary().unique().notNullable()
-    table.string('name').notNullable().unique()
+    table.string('name').notNullable()
     table.string('cpf').notNullable().unique()
     table.string('email').notNullable().unique()
   })
 
 exports.down = (knex) => knex.schema.dropTable('student')
-
