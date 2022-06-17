e90720d229f28efc0bacc715e5340f0ab993fcb6
viniciushrcs <viniciushrcs@gmail.com>
2022-06-17 16:35:09 -0300

    feat: creates getAll method in StudentRepository

--- src/infra/database/repositories/studentRepository.js
+++ src/infra/database/repositories/studentRepository.js
@@ -4,6 +4,18 @@ class StudentRepository {
   constructor() {
     this.knex = knex
   }
+
+  async getAll() {
+    try {
+      const response = await this.knex('student')
+      if (!response || response.length === 0) return null
+      return response
+    } catch (error) {
+      throw new Error(
+        'An error ocurred while trying to get all registers from student table'
+      )
+    }
+  }
 }
 
 module.exports = StudentRepository
