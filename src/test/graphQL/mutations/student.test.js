502db40c2121d79d1493b4fe24d2aba10178a212
viniciushrcs <viniciushrcs@gmail.com>
2022-06-17 18:33:21 -0300

    test: mutation createStudent should throws an error if cpf is not passed as parameter

---
+++ src/test/graphQL/mutations/student.test.js
@@ -0,0 +1,41 @@
+const { ApolloServer } = require('apollo-server')
+const { expect } = require('chai')
+const { describe, it } = require('mocha')
+const typeDefs = require('../../../app/graphql/_typeDefs/types')
+const { mutationsMock } = require('../../mocks')
+
+const {
+  studentMutationMocks: { createStudentValidReturn, createStudentNoCPFError },
+} = mutationsMock
+
+const resolversMock = {
+  Mutation: {
+    createStudent: () => createStudentValidReturn,
+  },
+}
+
+const testServer = new ApolloServer({
+  typeDefs,
+  resolvers: resolversMock,
+  mockEntireSchema: false,
+})
+
+describe('Student mutations', () => {
+  it('Should throws an error when createStudent is called without passing cpf', async () => {
+    const result = await testServer.executeOperation({
+      query: `mutation($cpf: String!, $name: String!, $email: String!) {
+        createStudent(cpf: $cpf, name: $name, email: $email) {
+          cpf
+          name
+          email
+        }
+      }`,
+      variables: {
+        name: 'name1',
+        email: 'email1',
+      },
+    })
+
+    expect(result.errors[0].message).to.be.equal(createStudentNoCPFError)
+  })
+})
