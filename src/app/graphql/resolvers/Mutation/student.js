3cc2f12d9e789c8352be6a1d08535ff6639f37ff
viniciushrcs <viniciushrcs@gmail.com>
2022-06-17 19:19:11 -0300

    feat: add validation of parameters in createStudent mutation

--- src/app/graphql/resolvers/Mutation/student.js
+++ src/app/graphql/resolvers/Mutation/student.js
@@ -3,14 +3,18 @@ const StudentRepository = require('../../../../infra/database/repositories/stude
 const studentRepository = new StudentRepository()
 
 const mutations = {
-  async createStudent(_, { name, cpf, email }) {
-    const newStudent = await studentRepository.createStudent({
+  async createStudent(_, newStudent) {
+    const { name, cpf, email } = newStudent
+    if (!name || !cpf || !email)
+      throw new Error('All parameters must be passed')
+
+    const newStudentResponse = await studentRepository.createStudent({
       name,
       cpf,
       email,
     })
 
-    return newStudent
+    return newStudentResponse
   },
 }
 
