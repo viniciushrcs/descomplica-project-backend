34321876e2977cfb683c10527f208dd9b190860b
viniciushrcs <viniciushrcs@gmail.com>
2022-06-19 14:07:20 -0300

    refactor: change student mutations resolver to use studentService

--- src/app/graphql/resolvers/Mutation/student.js
+++ src/app/graphql/resolvers/Mutation/student.js
@@ -1,53 +1,24 @@
-const StudentRepository = require('../../../../infra/database/repositories/studentRepository')
+const StudentService = require('../../services/studentService')
 
-const studentRepository = new StudentRepository()
+const studentService = new StudentService()
 
 const mutations = {
   async createStudent(_, newStudent) {
-    const { name, cpf, email } = newStudent
-    if (!name || !cpf || !email)
-      throw new Error('All parameters must be passed')
-
-    const newStudentResponse = await studentRepository.createStudent({
-      name,
-      cpf,
-      email,
-    })
+    const newStudentResponse = await studentService.createStudent(newStudent)
 
     return newStudentResponse
   },
 
-  async editStudent(_, updateStudent) {
-    const { name, cpf, email } = updateStudent
-    if (!name || !cpf || !email)
-      throw new Error('All parameters must be passed')
-
-    const editStudentResponse = await studentRepository.editStudent({
-      name,
-      cpf,
-      email,
-    })
+  async editStudent(_, student) {
+    const editStudentResponse = await studentService.editStudent(student)
 
     return editStudentResponse
   },
 
-  async deleteStudent(_, { cpf }) {
-    if (!cpf) throw new Error('CPF must be passed as parameter')
+  async deleteStudent(_, cpf) {
+    const deleteStudentResponse = await studentService.deleteStudent(cpf)
 
-    const studentToBeDeleted = await studentRepository.getStudent({
-      cpf,
-    })
-
-    if (!studentToBeDeleted)
-      throw new Error(`Could not find a student with cpf: ${cpf}`)
-
-    await studentRepository.deleteStudent({
-      cpf,
-      name: studentToBeDeleted.name,
-      email: studentToBeDeleted.email,
-    })
-
-    return true
+    return deleteStudentResponse
   },
 }
 
