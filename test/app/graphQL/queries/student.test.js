2c33f62055eae262a64a803ec8b20c05bab540e1
ViniciusChagas <viniciushrcs@gmail.com>
2022-06-20 20:05:51 -0300

    fix: fix student queries test

--- test/app/graphQL/queries/student.test.js
+++ test/app/graphQL/queries/student.test.js
@@ -1,53 +1,39 @@
-const { ApolloServer } = require('apollo-server')
 const { expect } = require('chai')
 const { describe, it } = require('mocha')
-const typeDefs = require('../../../../app/graphql/_typeDefs/types')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
 const { queriesMock } = require('../../../mocks')
-const { queryResolverMock } = require('../../../mocks/resolvers')
 
 const {
   studentQueryMocks: { getStudentValidReturn, getStudentsValidReturn },
 } = queriesMock
 
-const testServer = new ApolloServer({
-  typeDefs,
-  resolvers: queryResolverMock,
-  mockEntireSchema: false,
-})
-
 describe('Student queries', () => {
-  it('Should return all students in getStudents query', async () => {
-    const result = await testServer.executeOperation({
-      query: `query getStudents {
-        getStudents {
-          cpf
-          name
-          email
-        }
-      }`,
-    })
+  const studentServiceStub = sinon.stub()
 
-    expect(result.data.getStudents[0]).to.haveOwnProperty('cpf')
-    expect(result.data.getStudents[0]).to.haveOwnProperty('name')
-    expect(result.data.getStudents[0]).to.haveOwnProperty('email')
-    expect(result.data.getStudents).to.be.deep.equal(getStudentsValidReturn)
+  const query = proxyquire(
+    '../../../../src/app/graphql/resolvers/Query/student',
+    {
+      '../../services/studentService': studentServiceStub,
+    }
+  )
+  it('Should return all students in getStudents query', async () => {
+    studentServiceStub.prototype.getStudents = sinon
+      .stub()
+      .resolves(getStudentsValidReturn)
+
+    const response = await query.getStudents()
+
+    expect(response).to.be.deep.equal(getStudentsValidReturn)
   })
 
   it('Should return one student in getStudent query', async () => {
-    const result = await testServer.executeOperation({
-      query: `query getStudent {
-        getStudent(name: "nome" cpf: "111111111" email: "email@email.com") {
-          cpf
-          name
-          email
-        }
-      }
-      `,
-    })
+    studentServiceStub.prototype.getStudent = sinon
+      .stub()
+      .resolves(getStudentValidReturn)
 
-    expect(result.data.getStudent).to.haveOwnProperty('cpf')
-    expect(result.data.getStudent).to.haveOwnProperty('name')
-    expect(result.data.getStudent).to.haveOwnProperty('email')
-    expect(result.data.getStudent).to.be.deep.equal(getStudentValidReturn)
+    const response = await query.getStudent()
+
+    expect(response).to.be.deep.equal(getStudentValidReturn)
   })
 })
